fa8c5aa31eba9bc044d3fa7a1ae3edc8b13e6687
Helena Converse <helena.converse@wwt.com>
2020-05-21 11:49:19 -0500

    master - adds api service
    faking API since I want this to run locally per device

---
+++ src/utils/requests.js
@@ -0,0 +1,11 @@
+/* eslint-disable global-require */
+
+const request = (file) => new Promise((resolve) => {
+  // eslint-disable-next-line import/no-dynamic-require
+  const json = require(`./api/${file}.json`);
+  setTimeout(() => resolve(json), Math.random() * 1000);
+});
+
+export const listChallenges = () => request('index');
+
+export const getChallenge = (id) => request(id);
---
+++ src/utils/requests.test.js
@@ -0,0 +1,64 @@
+import * as faker from 'faker';
+import { listChallenges, getChallenge } from './requests';
+
+jest.useFakeTimers();
+
+describe('requests', () => {
+  describe('listChallenges', () => {
+    it('should be in the correct shape', (done) => {
+      listChallenges()
+        .then((result) => {
+          expect(result.challenges).toBeDefined();
+          const challenge = faker.random.arrayElement(result.challenges);
+          expect(challenge.title).toBeDefined();
+          expect(challenge.days).toBeDefined();
+          expect(challenge.link).toBeDefined();
+          expect(challenge.id).toBeDefined();
+          done();
+        });
+      jest.advanceTimersByTime(1000);
+    });
+  });
+
+  describe('list', () => {
+    const challengeId = faker.random.arrayElement([1]);
+    it('should have the correct top level shape', (done) => {
+      getChallenge(challengeId)
+        .then((result) => {
+          expect(result.challenge).toBeDefined();
+          expect(result.challenge.name).toBeDefined();
+          expect(result.challenge.days).toBeDefined();
+          // const day = faker.random.arrayElement(result.challenge);
+          // expect(day.day).toBeDefined();
+          // const exercise = faker.random.arrayElement(day.exercises);
+          // expect(exercise.name).toBeDefined();
+          // expect(exercise.name || exercise.reps).toBeDefined();
+          done();
+        });
+      jest.advanceTimersByTime(1000);
+    });
+
+    it('should have the correct days shape', (done) => {
+      getChallenge(challengeId)
+        .then((result) => {
+          const day = faker.random.arrayElement(result.challenge.days);
+          expect(day.day).toBeDefined();
+          expect(day.exercises).toBeDefined();
+          done();
+        });
+      jest.advanceTimersByTime(1000);
+    });
+
+    it('should have the correct exercise shape', (done) => {
+      getChallenge(challengeId)
+        .then((result) => {
+          const day = faker.random.arrayElement(result.challenge.days);
+          const exercise = faker.random.arrayElement(day.exercises);
+          expect(exercise.name).toBeDefined();
+          expect(exercise.name || exercise.reps).toBeDefined();
+          done();
+        });
+      jest.advanceTimersByTime(1000);
+    });
+  });
+});
